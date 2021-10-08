6c81575b6107ad8f6b4eaffaca0cc7168dfeef46
Grisha Starykh <grishafv@gmail.com>
2021-10-08 16:46:12 +0300

    1

--- main.ino
+++ main.ino
@@ -1,6 +1,49 @@
-#include "Mechanics.h"
+#define MOTOR_INTERFACE_TYPE 1 //1 означает внешний шаговый драйвер с выводами Step и Direction
 
-Mechanics mechanics;
+#define ACCELERATION 3000
+#define MAX_SPEED 60000
+
+
+#include <AccelStepper.h>
+
+class Motor {
+public:
+    int stepPin;
+    int dirPin;
+    int limitPin;
+    int stepsPerMm;
+
+    AccelStepper accelStepper;
+
+    Motor(int stepPin, int dirPin, int limitPin, int stepsPerMm) :
+            accelStepper(MOTOR_INTERFACE_TYPE, stepPin, dirPin) {
+    };
+
+    void setup() {
+        accelStepper.setMaxSpeed(MAX_SPEED);
+        pinMode(limitPin, INPUT);
+
+        accelStepper.setAcceleration(ACCELERATION);
+    };
+
+    void setHome() {
+        accelStepper.setAcceleration(300000000);
+
+        while (digitalRead(limitPin) == 1) {
+            accelStepper.moveTo(accelStepper.currentPosition() + (stepsPerMm * -0.3));
+            accelStepper.runSpeedToPosition();
+        };
+        accelStepper.setCurrentPosition(0);
+
+        accelStepper.setMaxSpeed(MAX_SPEED);
+    }
+
+    void moveTo(double distance) {
+        accelStepper.moveTo(int(stepsPerMm * distance));
+        accelStepper.runSpeedToPosition();
+    };
+
+};
 
 void setup() {
     mechanics.driverEnable();
