ac84fe6aabe234d2e3dd9d3d64fedbe36cb63610
Grisha Starykh <grishafv@gmail.com>
2021-10-08 16:52:07 +0300

    2

--- main.ino
+++ main.ino
@@ -3,6 +3,16 @@
 #define ACCELERATION 3000
 #define MAX_SPEED 60000
 
+#define CARRIAGE_STEP_PIN 2
+#define CARRIAGE_DIR_PIN 5
+#define CARRIAGE_LIMIT_PIN 9
+#define CARRIAGE_STEPS_PER_MM 3200
+
+#define TABLE_STEP_PIN 4
+#define TABLE_DIR_PIN 7
+#define TABLE_LIMIT_PIN 11
+#define TABLE_STEPS_PER_MM 800
+
 
 #include <AccelStepper.h>
 
@@ -45,20 +55,32 @@ public:
 
 };
 
+Motor carriage(CARRIAGE_STEP_PIN,
+               CARRIAGE_DIR_PIN,
+               CARRIAGE_LIMIT_PIN,
+               CARRIAGE_STEPS_PER_MM);
+
+Motor table(TABLE_STEP_PIN,
+            TABLE_DIR_PIN,
+            TABLE_LIMIT_PIN,
+            TABLE_STEPS_PER_MM);
+
+void driveEnable() {
+    pinMode(8, OUTPUT);
+    digitalWrite(8, 0);
+}
+
 void setup() {
-    mechanics.driverEnable();
+    driveEnable();
+    table.setup();
+    carriage.setup();
 
-    mechanics.table.setHome();
-    mechanics.carriage.setHome();
-
-    long positions[2];
-    positions[0] = 30000;
-    positions[1] = 10000;
-    mechanics.steppers.moveTo(positions);
-    mechanics.steppers.runSpeedToPosition();
+    table.setHome();
+    carriage.setHome();
 
 }
 
 void loop() {
-
+    carriage.moveTo(70);
+    table.moveTo(60);
 }
